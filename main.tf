c6496ffdfba73fb4a5c66363b8fd284cd93773e2
DiazGIT <cloud.dias@outlook.com>
2025-11-26 17:12:36 -0300

    Add Bucket block

--- main.tf
+++ main.tf
@@ -27,6 +27,24 @@ provider "aws" {
   region = "us-east-1"
 }
 
+resource "aws_s3_bucket" "hcp_bucket" {
+  bucket = "myhcpbucket001"
+
+  tags = {
+    Name        = "HCP Bucket"
+    Environment = "Dev"
+  }
+}
+
+resource "aws_s3_bucket_public_access_block" "hcp_bucket" {
+  bucket = aws_s3_bucket.hcp_bucket.id
+
+  block_public_acls       = false
+  block_public_policy     = false
+  ignore_public_acls      = false
+  restrict_public_buckets = false
+}
+
 resource "random_pet" "sg" {}
 
 resource "aws_ebs_volume" "example" {
