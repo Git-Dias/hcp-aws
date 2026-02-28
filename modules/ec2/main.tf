e6917e06e4616555808bb2a7396c46b32c9578b4
DiazGIT <cloud.dias@outlook.com>
2026-02-27 22:43:44 -0300

    fix: enable imdsv2

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -14,3 +14,8 @@ resource "aws_ebs_volume" "example" {
   size              = 8
   encrypted         = true
 }
+
+resource "aws_ec2_instance_metadata_defaults" "enforce-imdsv2" {
+  http_tokens                 = "required"
+  http_put_response_hop_limit = 1
+}
