7911f2f6a9271733f959c04c68403d5f575b128d
DiazGIT <cloud.dias@outlook.com>
2026-02-27 22:49:48 -0300

    fix: edit metadata_options

--- modules/ec2/main.tf
+++ modules/ec2/main.tf
@@ -4,6 +4,11 @@ resource "aws_instance" "this" {
   instance_type = var.instance_type
   subnet_id     = var.subnet_id
 
+  metadata_options {
+    http_tokens                 = "required"
+    http_put_response_hop_limit = 1
+  }
+
   tags = {
     Name = "lab-ec2-${count.index}"
   }
@@ -13,9 +18,4 @@ resource "aws_ebs_volume" "example" {
   availability_zone = "us-east-1a"
   size              = 8
   encrypted         = true
-}
-
-resource "aws_ec2_instance_metadata_defaults" "enforce-imdsv2" {
-  http_tokens                 = "required"
-  http_put_response_hop_limit = 1
 }
